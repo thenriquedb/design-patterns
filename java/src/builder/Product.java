d527e4e9bca8ba65fe015c4d10a9424c1741b508
thiago <thenrique2012@gmail.com>
2020-12-19 14:53:46 -0300

    Create builder example

---
+++ java/src/builder/Product.java
@@ -0,0 +1,22 @@
+package builder;
+
+public class Product {
+    private String name;
+    private Float price;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+}
